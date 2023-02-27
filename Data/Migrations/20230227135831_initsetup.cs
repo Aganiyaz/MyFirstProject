4c0a6320fce24a060c44c673c6d37d55b2658c88
Aganiyaz <aganiyazparkhatov@gmail.com>
2023-02-27 20:24:01 +0500

    first commit

---
+++ Data/Migrations/20230227135831_initsetup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace appjoke.Data.Migrations
+{
+    public partial class initsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "joke",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    JokeQuestion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JokeAnswer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_joke", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "joke");
+        }
+    }
+}
---
+++ Models/joke.cs
@@ -0,0 +1,16 @@
+﻿namespace appjoke.Models
+{
+    public class joke
+    {
+
+        public int ID { get; set; }
+
+        public string JokeQuestion  { get; set; }
+        public string  JokeAnswer { get; set; }
+
+        public joke()
+        {
+
+        }
+    }
+}
